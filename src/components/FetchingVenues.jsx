3d327666fe533db18f13112fafeeb4755427a190
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-09-04 10:47:32 +0200

    Content on venues
    Added more content to Venues, also split up the code into 2 files.

---
+++ src/components/FetchingVenues.jsx
@@ -0,0 +1,208 @@
+import React, { useState, useEffect } from "react";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faWifi,
+  faCar,
+  faDog,
+  faCoffee,
+} from "@fortawesome/free-solid-svg-icons";
+
+const VenuesContainer = styled.div`
+  background-color: ${(props) => props.theme.color.c5};
+  border-radius: 10px;
+  padding: 10px 30px;
+`;
+
+const VenuesContent = styled.div`
+  display: flex;
+  border-radius: 10px;
+  margin: 20px 0px;
+  justify-content: space-between;
+  box-shadow: 0px 0px 6px rgb(239, 243, 246);
+  background-color: white;
+  transition: color 0.3s, transform 0.3s;
+
+  &:hover {
+    background-color: ${(props) => props.theme.color.c2};
+    transform: scale(1.01);
+  }
+`;
+
+const VenueName = styled.h2`
+  font-size: 1.2rem;
+  margin: 0px;
+  color: ${(props) => props.theme.color.c3};
+`;
+
+const VenueImage = styled.div`
+  width: 300px;
+  height: 300px;
+  border-radius: 5px;
+  overflow: hidden;
+`;
+
+const Featimg = styled.img`
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+`;
+
+const VenueText = styled.div`
+  padding: 5px 20px;
+  color: ${(props) => props.theme.color.c4};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 200px;
+`;
+
+const VenueCountry = styled.span``;
+
+const Metas = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+const IconColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const TextColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 10px;
+  font-size: 0.8rem;
+`;
+
+const IconSpace = styled.div`
+  padding: 8px 0;
+`;
+
+const IconTextSpace = styled.div`
+  padding: 5px 0;
+`;
+
+const VenueMeta = styled.div`
+  color: ${(props) => props.theme.color.c3};
+`;
+
+const VenueDescription = styled.p`
+  width: 500px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  color: ${(props) => props.theme.color.c4};
+`;
+
+const VenuePrice = styled.p`
+  display: flex;
+  align-items: flex-end;
+  margin: 16px;
+  padding: 5px;
+  font-size: 1.2rem;
+  color: ${(props) => props.theme.color.c6};
+`;
+
+const CardSplitter = styled.div`
+  display: flex;
+`;
+
+function FetchVenues() {
+  const url = "https://api.noroff.dev/api/v1/holidaze/venues";
+
+  const [venues, setVenues] = useState([]);
+  useEffect(() => {
+    async function getVenues() {
+      const response = await fetch(url);
+      const json = await response.json();
+      setVenues(json);
+    }
+    getVenues();
+  }, []);
+
+  const maxDescriptionSize = (description) => {
+    const maxDescriptionLength = 200;
+    return description.length > maxDescriptionLength
+      ? description.slice(0, maxDescriptionLength) + "..."
+      : description;
+  };
+
+  return (
+    <VenuesContainer>
+      {venues.map((venue) => (
+        <Link to={`/Venue/${venue.id}`} key={venue.id}>
+          <VenuesContent>
+            <CardSplitter>
+              <VenueImage>
+                <Featimg
+                  src={venue.media}
+                  alt={venue.name}
+                  onError={(e) => {
+                    e.target.src =
+                      "https://i.ytimg.com/vi/x6WcmrcQyaM/maxresdefault.jpg";
+                  }}
+                ></Featimg>
+              </VenueImage>
+              <VenueText>
+                <div>
+                  <VenueName>{venue.name}</VenueName>
+                  <VenueCountry>{venue.location.country}</VenueCountry>
+                </div>
+                <VenueMeta>
+                  <Metas>
+                    <IconColumn>
+                      <IconTextSpace>
+                        <FontAwesomeIcon icon={faUser} />
+                      </IconTextSpace>
+                      {venue.meta.wifi && (
+                        <IconTextSpace>
+                          <FontAwesomeIcon icon={faWifi} />
+                        </IconTextSpace>
+                      )}
+                      {venue.meta.parking && (
+                        <IconTextSpace>
+                          <FontAwesomeIcon icon={faCar} />
+                        </IconTextSpace>
+                      )}
+                      {venue.meta.breakfast && (
+                        <IconTextSpace>
+                          <FontAwesomeIcon icon={faCoffee} />
+                        </IconTextSpace>
+                      )}
+                      {venue.meta.pets && (
+                        <IconTextSpace>
+                          <FontAwesomeIcon icon={faDog} />
+                        </IconTextSpace>
+                      )}
+                    </IconColumn>
+                    <TextColumn>
+                      <IconSpace> {venue.maxGuests}</IconSpace>
+                      {venue.meta.wifi && <IconSpace>Wifi</IconSpace>}
+                      {venue.meta.parking && <IconSpace>Parking</IconSpace>}
+                      {venue.meta.breakfast && <IconSpace>Breakfast</IconSpace>}
+                      {venue.meta.pets && <IconSpace>Pets</IconSpace>}
+                    </TextColumn>
+                  </Metas>
+                </VenueMeta>
+              </VenueText>
+            </CardSplitter>
+            <VenueDescription>
+              {maxDescriptionSize(venue.description)}
+            </VenueDescription>
+            <VenuePrice>
+              <b>Price: £{venue.price}</b>
+            </VenuePrice>
+          </VenuesContent>
+        </Link>
+      ))}
+    </VenuesContainer>
+  );
+}
+
+export default FetchVenues;
--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { styled } from "styled-components";
-import { Link } from "react-router-dom";
 import SearchComponent from "../components/SearchComponent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faWifi,
-  faCar,
-  faDog,
-  faCoffee,
-} from "@fortawesome/free-solid-svg-icons";
+import { faSliders } from "@fortawesome/free-solid-svg-icons";
+import FetchVenues from "../components/FetchingVenues";
 
 const VenuesSection = styled.section`
   background-color: ${(props) => props.theme.color.c2};
@@ -21,172 +15,45 @@ const VenuesWidth = styled.div`
   padding: 10px;
 `;
 
-const VenuesContainer = styled.div`
-  background-color: ${(props) => props.theme.color.c5};
-  border-radius: 10px;
-  padding: 30px;
-`;
-
-const VenuesContent = styled.div`
-  display: flex;
-  border-radius: 10px;
-  margin: 10px;
-
-  &:hover {
-    background-color: ${(props) => props.theme.color.c2};
-  }
-`;
-
 const SiteTitle = styled.div`
-  padding: 20px;
-`;
-
-const VenueName = styled.h2`
-  font-size: 1.2rem;
-  margin: 0px;
-  color: ${(props) => props.theme.color.c3};
-`;
-
-const VenueImage = styled.div`
-  width: 300px;
-  height: 300px;
-  border-radius: 5px;
-  overflow: hidden;
-`;
-
-const Featimg = styled.img`
-  width: 300px;
-  height: 300px;
-  object-fit: cover;
+  padding: 5px 10px;
 `;
 
 const H1 = styled.h1`
   margin-bottom: 2px;
 `;
 
-const VenueText = styled.div`
-  padding: 0px 20px;
-  color: ${(props) => props.theme.color.c4};
+const FilterDiv = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: space-between;
+  padding: 5px;
 `;
 
-const VenueCountry = styled.span``;
-
-const Metas = styled.div`
+const FilterVenues = styled.div`
+  padding: 5px;
+  margin: 10px;
   display: flex;
-  align-items: flex-start;
-`;
-
-const IconColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`;
-
-const TextColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-left: 10px;
-`;
-
-const IconSpace = styled.div`
-  padding: 5px 0;
-`;
-
-const VenueMeta = styled.div`
-  color: ${(props) => props.theme.color.c3};
+  align-items: flex-end;
 `;
 
 function Venues() {
-  const url = "https://api.noroff.dev/api/v1/holidaze/venues";
-
-  const [venues, setVenues] = useState([]);
-  useEffect(() => {
-    async function getVenues() {
-      const response = await fetch(url);
-      const json = await response.json();
-      setVenues(json);
-    }
-    getVenues();
-  }, []);
-
   return (
     <VenuesSection>
       <SearchComponent />
       <VenuesWidth>
-        <SiteTitle>
-          <H1>Venues</H1>
-          <span>A list of all selected venues</span>
-        </SiteTitle>
-        <VenuesContainer>
-          {venues.map((venue) => (
-            <Link to={`/Venue/${venue.id}`} key={venue.id}>
-              <VenuesContent>
-                <VenueImage>
-                  <Featimg
-                    src={venue.media}
-                    alt={venue.name}
-                    onError={(e) => {
-                      e.target.src =
-                        "https://i.ytimg.com/vi/x6WcmrcQyaM/maxresdefault.jpg";
-                    }}
-                  ></Featimg>
-                </VenueImage>
-                <VenueText>
-                  <div>
-                    <VenueName>{venue.name}</VenueName>
-                    <VenueCountry>{venue.location.country}</VenueCountry>
-                  </div>
-
-                  <VenueMeta>
-                    <Metas>
-                      <IconColumn>
-                        <IconSpace>
-                          <FontAwesomeIcon icon={faUser} />
-                        </IconSpace>
-                        {venue.meta.wifi && (
-                          <IconSpace>
-                            <FontAwesomeIcon icon={faWifi} />
-                          </IconSpace>
-                        )}
-                        {venue.meta.parking && (
-                          <IconSpace>
-                            <FontAwesomeIcon icon={faCar} />
-                          </IconSpace>
-                        )}
-                        {venue.meta.breakfast && (
-                          <IconSpace>
-                            <FontAwesomeIcon icon={faCoffee} />
-                          </IconSpace>
-                        )}
-                        {venue.meta.pets && (
-                          <IconSpace>
-                            <FontAwesomeIcon icon={faDog} />
-                          </IconSpace>
-                        )}
-                      </IconColumn>
-                      <TextColumn>
-                        <IconSpace> {venue.maxGuests}</IconSpace>
-
-                        {venue.meta.wifi && <IconSpace>Wifi</IconSpace>}
-                        {venue.meta.parking && <IconSpace>Parking</IconSpace>}
-                        {venue.meta.breakfast && (
-                          <IconSpace>Breakfast</IconSpace>
-                        )}
-                        {venue.meta.pets && <IconSpace>Pets</IconSpace>}
-                      </TextColumn>
-                    </Metas>
-                  </VenueMeta>
-                </VenueText>
-              </VenuesContent>
-            </Link>
-          ))}
-        </VenuesContainer>
+        <FilterDiv>
+          <SiteTitle>
+            <H1>Venues</H1>
+            <span>A list of all selected venues</span>
+          </SiteTitle>
+          <FilterVenues>
+            <FontAwesomeIcon icon={faSliders} />
+          </FilterVenues>
+        </FilterDiv>
+        <FetchVenues />
       </VenuesWidth>
     </VenuesSection>
   );
 }
+
 export default Venues;
