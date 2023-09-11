6028f6fb2bfb866dc426d1f7a08049c1f7d8ee0c
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-09-11 12:43:27 +0200

    content venue specific

--- src/pages/Venue.jsx
+++ src/pages/Venue.jsx
@@ -1,6 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchVenue } from "../API/apiVenues";
+import styled from "styled-components";
+
+const VenueContainer = styled.div`
+  background-color: ${(props) => props.theme.color.c2};
+  height: 100%;
+`;
+
+const FirstContainer = styled.div`
+  width: 30%;
+`;
+
+const H1 = styled.h1``;
+
+const Country = styled.span`
+  display: flex;
+`;
+
+const Location = styled.span``;
+
+const SecondContainer = styled.div``;
 
 function Venue() {
   const [venueData, setVenueData] = useState(null);
@@ -16,16 +36,33 @@ function Venue() {
       });
   }, [id]);
 
+  console.log(venueData);
+
   return (
-    <div>
+    <VenueContainer>
+      <FirstContainer></FirstContainer>
       {venueData ? (
         <>
-          <h1>{venueData.name}</h1>
+          <FirstContainer>
+            <H1>{venueData.name}</H1>
+            <Country>{venueData.location.country} </Country>
+            {venueData.location.zip &&
+              venueData.location.address !== "Unknown" && (
+                <Location>
+                  {venueData.location.address} - {venueData.location.zip}
+                </Location>
+              )}
+            {venueData.location.city !== "Unknown" && (
+              <Location> {venueData.location.city}</Location>
+            )}
+            <hr></hr>
+          </FirstContainer>
+          <SecondContainer></SecondContainer>
         </>
       ) : (
         <p>Loading...</p>
       )}
-    </div>
+    </VenueContainer>
   );
 }
 
