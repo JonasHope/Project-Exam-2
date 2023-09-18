17b760ca72bcfdc54a1ef95654727ec007f53dc8
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-09-18 11:52:39 +0200

    fetch works

---
+++ src/API/apiUsers.js
@@ -0,0 +1,40 @@
+import { BASE_URL } from "./apiConsts";
+
+const Profile_URL = "/profiles";
+
+const userString = localStorage.getItem("user");
+const user = JSON.parse(userString);
+const fetchName = user?.name
+
+export async function fetchProfile() {
+
+  const name = fetchName;
+
+  const url = `${BASE_URL}${Profile_URL}/${name}?`;
+
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (!accessToken) {
+    throw new Error("Access token not found in local storage.");
+  }
+
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    
+    "Content-Type": "application/json", 
+  };
+
+  const requestOptions = {
+    method: "GET", 
+    headers,
+  };
+
+  const response = await fetch(url, requestOptions);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch booking: ${response.statusText}`);
+  }
+
+  const json = await response.json();
+  return json;
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Width from "../styles/Width";
+import { fetchProfile } from "../API/apiUsers";
 
 const ProfileContainer = styled.div`
   padding: 10px;
 `;
 
 function ProfilePage() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const userData = await fetchProfile();
+
+        setUser(userData);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+
+    fetchUser();
+  }, []);
+
   return (
     <ProfileContainer>
       <Width>
-        <h1>Name</h1>
+        <div>
+          <h1>{user.name}</h1>
+        </div>
       </Width>
     </ProfileContainer>
   );
