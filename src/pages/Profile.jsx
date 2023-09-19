44abdcbf4ae7124983362283d86c8a5b4a0966df
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-09-19 11:08:48 +0200

    profile info is in place.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { StyleSheetManager } from "styled-components";
 import styled from "styled-components";
 import Width from "../styles/Width";
 import { fetchProfile } from "../API/apiUsers";
@@ -7,19 +8,68 @@ const ProfileContainer = styled.div`
   padding: 10px;
 `;
 
-const ProfileInfo = styled.div`
+const ProfileInfoContainer = styled.div`
+  display: flex;
+  align-items: flex-top;
+  margin: 20px 0px;
+`;
+
+const ProfileImageContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-const ProfileImageContainer = styled.div`
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0px 0px 10px;
+`;
+
+const H1 = styled.h1`
+  margin: 5px 0px;
+  font-size: 1.3rem;
+`;
+
+const EmailProfile = styled.span`
+  font-size: 0.9rem;
+  color: ${(props) => props.theme.color.c4};
+`;
+
+const VM = styled.span`
+  color: ${(props) => props.theme.color.c6};
+`;
+
+const ProfileImage = styled.div`
   background-image: url(${(props) => props.image});
-  width: 100px;
-  height: 100px;
+  width: 120px;
+  height: 120px;
   border-radius: 50%;
   background-repeat: no-repeat, no-repeat;
   background-position: center;
   background-size: cover;
+`;
+
+const ChangeAvatarForm = styled.form`
+  margin-top: 5px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ChangeAvatarInput = styled.input`
+  max-width: 120px;
+  padding: 5px;
+  border: none;
+  border-bottom: 1px solid ${(props) => props.theme.color.c6};
+  background-color: inherit;
+`;
+
+const ChangeAvatarButton = styled.button`
+  margin-top: 5px;
+  border: none;
+  border-radius: 2px;
+  padding: 5px 10px;
+  background-color: ${(props) => props.theme.color.c3};
+  color: ${(props) => props.theme.color.c5};
 `;
 
 function ProfilePage() {
@@ -41,28 +91,37 @@ function ProfilePage() {
   }, []);
 
   useEffect(() => {
-    if (user.avatar > 0) {
-      setProfileImage(user?.avatar);
-    }
+    setProfileImage(user?.avatar);
   }, [user?.avatar]);
 
   return (
-    <ProfileContainer>
-      <Width>
-        {user ? (
-          <>
-            <ProfileImageContainer image={profileImage}></ProfileImageContainer>
-            <ProfileInfo>
-              <h1>{user.name}</h1>
-              <span>{user.email}</span>
-              <span>{user.venueManager && <span>Venue Manager</span>}</span>
-            </ProfileInfo>
-          </>
-        ) : (
-          <p>(temp, Remember to add loader here)</p>
-        )}
-      </Width>
-    </ProfileContainer>
+    <StyleSheetManager shouldForwardProp={(prop) => !["image"].includes(prop)}>
+      <ProfileContainer>
+        <Width>
+          {user ? (
+            <ProfileInfoContainer>
+              <ProfileImageContainer>
+                <ProfileImage image={profileImage}></ProfileImage>
+                <ChangeAvatarForm>
+                  <ChangeAvatarInput
+                    type="text"
+                    placeholder="Image URL"
+                  ></ChangeAvatarInput>
+                  <ChangeAvatarButton>Change Avatar</ChangeAvatarButton>
+                </ChangeAvatarForm>
+              </ProfileImageContainer>
+              <ProfileInfo>
+                <H1>{user.name}</H1>
+                <EmailProfile>{user.email}</EmailProfile>
+                <VM>{user.venueManager && <span>Venue Manager</span>}</VM>
+              </ProfileInfo>
+            </ProfileInfoContainer>
+          ) : (
+            <p>(temp, Remember to add loader here)</p>
+          )}
+        </Width>
+      </ProfileContainer>
+    </StyleSheetManager>
   );
 }
 
