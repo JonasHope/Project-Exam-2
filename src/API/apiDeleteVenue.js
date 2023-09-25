eeb05a3d4e4c5a07d32ad54b7ed0bdca544cf798
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-09-25 12:29:42 +0200

    adding deleting of venues

---
+++ src/API/apiDeleteVenue.js
@@ -0,0 +1,37 @@
+import { BASE_URL } from "./apiConsts";
+
+const Venue_URL = "/venue"
+
+export async function deleteBooking(id) {
+  if (!id) {
+    throw new Error("Missing venue ID");
+  }
+
+  const url = `${BASE_URL}${Venue_URL}/${id}`;
+
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (!accessToken) {
+    throw new Error("Access token not found in local storage.");
+  }
+
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    
+    "Content-Type": "application/json", 
+  };
+
+  const requestOptions = {
+    method: "DELETE", 
+    headers,
+  };
+
+  const response = await fetch(url, requestOptions);
+
+  if (!response.ok) {
+    throw new Error(`Failed to delete booking: ${response.statusText}`);
+  }
+
+  
+  return 
+}
